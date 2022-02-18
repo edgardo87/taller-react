f0777c6790cae71130c4d1662e4d6c49fa4832ce
Edgardo De Avila <edgardodeavila@ingenieros.com>
2022-02-17 20:04:00 -0500

    Prueba utilización componente Personaje

--- src/componentes/Navegacion.jsx
+++ src/componentes/Navegacion.jsx
@@ -1,9 +1,12 @@
+import Personaje from "./Personaje.jsx";
 
 function Navegacion() {
 
     return (
         <div>
-            <p>Adrian De Avila</p>
+            <Personaje name="Edgardo De Avila" genero="Masculino"/>
+            <Personaje name="Angela Quintero" genero="Femenino"/>
+            <Personaje name="Adrian De Avila" genero="Masculino"/>
         </div>
     );
 }
