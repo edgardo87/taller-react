070c8a5166798d7795825069e8f245d68dd9f012
Edgardo De Avila <edgardodeavila@ingenieros.com>
2022-02-17 19:56:50 -0500

    creación componente personaje

---
+++ src/componentes/Personaje.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Personaje(props) {
+
+    return (
+        <div>
+            <p><b>Nombre:</b> {props.name}</p>
+            <p><b>Genero:</b> {props.genero}</p>
+            <img alt='imagen-personaje' src={props.foto}></img>
+        </div>
+    );
+}
+
+export default Personaje;
